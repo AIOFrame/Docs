0fccbc803ab5a556d6b32be8a61ecc5ebc4bebf8
shaikh@utsme.com <Shaikh1234>
2019-03-04 14:36:50 +0400

    Minor Fixes

--- assets/scripts/docs.js
+++ assets/scripts/docs.js
@@ -5,4 +5,32 @@ $(document).ready(function(){
 
     Barba.Pjax.start();
 
+    Barba.Pjax.getTransition = function() {
+        return FadeTransition;
+    };
+
+});
+
+var FadeTransition = Barba.BaseTransition.extend({
+    start: function() {
+        Promise
+            .all([this.newContainerLoading, this.fadeOut()])
+            .then(this.fadeIn.bind(this));
+    },
+    fadeOut: function() {
+        return $(this.oldContainer).animate({ opacity: 0 }).promise();
+    },
+    fadeIn: function() {
+        var _this = this;
+        var $el = $(this.newContainer);
+        $(this.oldContainer).hide();
+        $($el).find('article').addClass('on');
+        $el.css({
+            visibility : 'visible',
+            opacity : 0
+        });
+        $el.animate({ opacity: 1 }, 400, function() {
+            _this.done();
+        });
+    }
 });
