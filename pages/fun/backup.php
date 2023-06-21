8315ffb6920dec201673a681846e78cabc3a1ed0
Shaikh Moinuddin <shaikh@aioframe.com>
2023-06-21 17:21:24 +0400

    Content Updates

--- pages/fun/backup.php
+++ pages/fun/backup.php
@@ -2,6 +2,20 @@
 get_comp('head');
 render_docs([
     [ 'h1', 'BACKUP()' ],
+    [ 'p', 'AIO gives you functionality to backup or restore your full database' ],
+    '.pb30',
+    [ 'h2', 'Pre Requisite' ],
+    [ 'p', 'Make sure you add \'backup\' in features in your web app config' ],
+    [ 'p', docs_config() ],
+    '.pb30',
+    [ 'h2', 'Initiate' ],
+    [ 'p', 'Call the BACKUP() class and assign it to any variable' ],
+    [ 'code', '$bk = new BACKUP();' ],
+    '.pb30',
+    [ 'h2', 'Usage' ],
+    'plain',
+    [ 'tr', [ 'Function' ] ],
+    'plain',
 ]);
 docs_nav();
 get_comp('foot');
